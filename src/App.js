57618a9f5422df26e9d510c1a58e48a50df6b7a9
jihye <kwisdom95@naver.com>
2024-02-14 02:52:52 +0900

    메인

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 export default function App() {
   return (
-    <div className="w-full h-[600px] bg-pink-200">
-      <div className="w-full h-[50px] bg-blue-200 text-center pt-3">문화 유산</div>
-      <div className="w-full h-full bg-green-100"></div>
+    <>
+    <div className="w-full h-[600px]">
+      <div className="w-full h-[50px] bg-slate-500 text-center pt-3 font-bold text-white">문화 유산</div>
+      <div className="w-full h-[550px]"><img className="w-full h-full object-cover object-center" src="image/main.jpg"/></div>
     </div>
+    <div className="max-w-7xl h-[500px] bg-blue-200 ml-[130px] flex">
+      <div className="mt-5 flex space-x-10">
+        <div className="w-[350px] h-[200px] bg-pink-200 ml-20"></div>
+      </div>
+    </div>
+    
+    </>
   )
 }
