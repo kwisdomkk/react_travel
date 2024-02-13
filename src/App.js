ca76476d3402daae506ca8e442f3a0d6a2dc811a
jihye <kwisdom95@naver.com>
2024-02-13 12:58:03 +0900

    s

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-
-
-function App() {
+export default function App() {
   return (
-    <div>추천여행지</div>
-  );
+    <div className="w-full h-[600px] bg-pink-200">
+      <div className="w-full h-[50px] bg-blue-200 text-center pt-3">문화 유산</div>
+      <div className="w-full h-full bg-green-100"></div>
+    </div>
+  )
 }
-
-export default App;
